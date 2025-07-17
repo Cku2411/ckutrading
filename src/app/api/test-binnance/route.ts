36394414d865261c8ed69fa6007f672280d9d204
cku <xidanhgiaychuan@gmail.com>
2025-07-17 22:52:13 +0700

    test binnac Use

--- src/app/api/test-binnance/route.ts
+++ src/app/api/test-binnance/route.ts
@@ -5,13 +5,17 @@ import axios from "axios";
 export async function GET() {
   try {
     const { data } = await axios.get(
-      "https://api.binance.com/api/v3/ticker/price"
+      "https://api.binance.us/api/v3/ticker/price"
     );
-    return NextResponse.json({ ok: true, count: data.length });
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json({
-      ok: false,
-    });
+    return NextResponse.json({ prices: data });
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      console.error(
+        "Binance.US error:",
+        err.response?.data,
+        err.response?.status
+      );
+    }
+    return new NextResponse("Failed to fetch prices", { status: 500 });
   }
 }
